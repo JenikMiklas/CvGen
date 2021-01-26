9e93fa71031bd89eadab9d698440add8035ef792
JenikMiklas <jenikmiklas@gmail.com>
2021-01-26 11:13:47 +0100

    Initial Commit

---
+++ Shared/CvGenApp.swift
@@ -0,0 +1,20 @@
+//
+//  CvGenApp.swift
+//  Shared
+//
+//  Created by Jan Miklas on 26.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct CvGenApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
