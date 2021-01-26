16b218c6d54494e556911fa8667294c48d8598c2
JenikMiklas <jenikmiklas@gmail.com>
2021-01-26 14:32:57 +0100

    Added source and test files

--- Shared/CvGenApp.swift
+++ Shared/CvGenApp.swift
@@ -13,8 +13,9 @@ struct CvGenApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            /*ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)*/
+            Home()
         }
     }
 }
---
+++ Shared/Views/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  CvGen
+//
+//  Created by Jan Miklas on 26.01.2021.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView(frame: .zero)
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            //webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+            webView.loadHTMLString(HTMLString.htmlString, baseURL: url)
+        } else {
+            print("unable to load template")
+        }
+        return webView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+}
