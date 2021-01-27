280cae9ca234045191403bf6539fdbff90d2a457
JenikMiklas <jenikmiklas@gmail.com>
2021-01-27 22:53:24 +0100

    Test Person View Card added.

---
+++ Shared/Views/TestPersonView.swift
@@ -0,0 +1,47 @@
+//
+//  TestPersonView.swift
+//  CvGen (iOS)
+//
+//  Created by Jan Miklas on 27.01.2021.
+//
+
+import SwiftUI
+
+struct TestPersonView: View {
+    
+    let person: Person
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Basic info")) {
+                Text("about")
+                Text("Name")
+                Text("Job")
+                Text("tel")
+                Text("e-mail")
+                Text("web")
+            }
+            Section(header: Text("Skills")) {
+                Text("skill, skill")
+            }
+            Section(header: Text("Socials")) {
+                Text("social, social")
+            }
+            Section(header: Text("Jobs")) {
+                Text("work, work")
+            }
+            Section(header: Text("Education")) {
+                Text("edu, edu")
+            }
+            Section(header: Text("Hobbies")) {
+                Text("hobby, hobby")
+            }
+        }
+    }
+}
+
+struct TestPersonView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestPersonView(person: Person())
+    }
+}
