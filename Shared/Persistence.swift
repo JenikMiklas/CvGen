3d4d26bef459786944740eaa750dc938c120b213
JenikMiklas <jenikmiklas@gmail.com>
2021-01-27 22:02:49 +0100

    Clean core date and ContentView.

--- Shared/Persistence.swift
+++ Shared/Persistence.swift
@@ -14,8 +14,8 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newPerson = Person(context: viewContext)
+            newPerson.name = "Jeník Mikláš"
         }
         do {
             try viewContext.save()
