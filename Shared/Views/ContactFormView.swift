3002a76d897b33d1d1d53c82ecc3961b59a24d56
JenikMiklas <jenikmiklas@gmail.com>
2021-02-02 12:45:40 +0100

    Contact form View created.

---
+++ Shared/Views/ContactFormView.swift
@@ -0,0 +1,23 @@
+//
+//  ContactFormView.swift
+//  CvGen (iOS)
+//
+//  Created by Jan Miklas on 02.02.2021.
+//
+
+import SwiftUI
+
+struct ContactFormView: View {
+    
+    @Binding var person: Person
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactFormView(person: .constant(Person()))
+    }
+}
