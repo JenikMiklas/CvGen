130d2cbdc7032fbf32e30f631d4532d5e9ba3a2b
JenikMiklas <jenikmiklas@gmail.com>
2021-02-03 21:58:20 +0100

    New View file.

---
+++ Shared/Views/ContainerViews.swift
@@ -0,0 +1,89 @@
+//
+//  ContainerViews.swift
+//  CvGen (iOS)
+//
+//  Created by Jan Miklas on 03.02.2021.
+//
+
+import SwiftUI
+
+struct ContetView: View {
+    
+    let header: String
+    let text: String
+    let from: Date
+    let to: Date
+    
+    var body: some View {
+        VStack {
+            Text(header)
+                .font(.title2)
+            Divider()
+            Text(text)
+            Divider()
+            HStack {
+                Text(DateFormatter.localizedString(from: from, dateStyle: .short, timeStyle: .none))
+                Spacer()
+                Text(DateFormatter.localizedString(from: to, dateStyle: .short, timeStyle: .none))
+            }
+        }
+        .padding()
+        .background(Color.gray)
+        .cornerRadius(10)
+    }
+}
+
+struct NewContetView: View {
+    
+    @EnvironmentObject var cvgVM: CvGenViewModel
+    
+    @Binding var showSheet: Bool
+    
+    @State private var header = ""
+    @State private var content = "Description"
+    @State private var from = Date()
+    @State private var to = Date()
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Company name", text: $header)
+                    TextEditor(text: $content)
+                        .opacity(content == "Description" ? 0.25 : 1)
+                        .frame(height: 300)
+                    HStack {
+                        DatePicker("from", selection: $from, displayedComponents: .date)
+                        DatePicker("to", selection: $to, displayedComponents: .date)
+                    }
+                    .font(.caption)
+                    Button(action: {
+                        if cvgVM.selectedObject != nil {
+                            cvgVM.updateContent(header: header, content: content, from: from, to: to)
+                        } else {
+                            cvgVM.createContent(header: header, content: content, from: from, to: to)
+                        }
+                        showSheet.toggle()
+                    }, label: {
+                        Text(cvgVM.selectedObject != nil ? "Update job" : "Add job")
+                    })
+                }
+            }
+            .onAppear {
+                let data = cvgVM.loadContent()
+                header = data.0
+                content = data.1
+                from = data.2
+                to = data.3
+            }
+            .navigationBarTitle("Insert new Job record", displayMode: .inline)
+            .navigationBarItems(trailing: Button(action: {
+                cvgVM.selectedObject = nil
+                showSheet.toggle()
+                
+            }, label: {
+                Text("Close")
+            }))
+        }
+    }
+}
